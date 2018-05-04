f9523c58c9e1cf6bbfc479cb310cc82e5c4610ca
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-04 10:50:49 -0600

    Add restocking test.

--- test/pantry_test.js
+++ test/pantry_test.js
@@ -16,5 +16,17 @@ describe("Pantry", () => {
     it("doesn't magically have cheese", () => {
       expect(pantry.stockCheck("Cheese")).to.eql(0)
     })
+
+    it("cheese must be added to pantry", () => {
+      pantry.restock("Cheese", 10)
+
+      expect(pantry.stock).to.eql({ "Cheese": 10 })
+    })
+
+    it("infinate amounts of cheese can be added", () => {
+      pantry.restock("Cheese", 20)
+
+      expect(pantry.stockCheck("Cheese")).to.eql(30)
+    })
   })
 })
